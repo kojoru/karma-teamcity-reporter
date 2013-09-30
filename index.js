5d70e4ed3be1794115c6ac8db276b14dc8d34138
Konstantin Yakushev <kojo@kojo.ru>
2013-09-30 15:47:49 +0400

    Replace literal \n with teamcity new lines

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var escapeMessage = function (message) {
     replace(/\|/g, '||').
     replace(/\'/g, '|\'').
     replace(/\n/g, '|n').
+    replace(/\\n/g, '|n').
     replace(/\r/g, '|r').
     replace(/\u0085/g, '|x').
     replace(/\u2028/g, '|l').
